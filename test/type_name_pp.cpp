1be47ddd1a18f32d9280e2e77c00cdfb2d834e0f
willwray <wjwray@gmail.com>
2019-06-26 10:00:43 -0400

    MSVC fix tests

--- test/type_name_pp.cpp
+++ test/type_name_pp.cpp
@@ -38,13 +38,13 @@ using namespace ltl;
 #endif
 
 static_assert(std::is_same_v<decltype(type_name_pp<char>), const ntbs::array<5>>);
-static_assert( type_name_pp<char> == ntbs::array{"char"} );
+static_assert( type_name_pp<char> == "char" );
 
 #define PP_SAME_T(type) \
 static_assert(type_name_pp<type> == #type)
 
 #define PP_GCM_T(type,G,C,M) \
-static_assert(type_name_pp<type> == GCM(#G,#C,#M))
+static_assert(type_name_pp<type> == GCM(G,C,M))
 
 // Simple type template args
 PP_SAME_T(char);
@@ -53,78 +53,81 @@ PP_SAME_T(unsigned char);
 
 PP_SAME_T(int);
 PP_SAME_T(unsigned int);
-static_assert(type_name_pp<signed int> == ntbs::array{"int"});
+
+static_assert(type_name_pp<signed int> == "int");
 
 PP_SAME_T(const int);
 PP_SAME_T(volatile int);
 
 PP_GCM_T(long
-        ,long int  // GCC
-        ,long      // Clang
-        ,long);    // MSVC
+       ,"long int" // GCC
+       ,"long"     // Clang
+       ,"long");   // MSVC
 
 PP_GCM_T(const volatile int
-        ,const volatile int      // GCC
-        ,const volatile int      // Clang
-        ,volatile const int);    // MSVC
+       ,"const volatile int"   // GCC
+       ,"const volatile int"   // Clang
+       ,"volatile const int"); // MSVC
 
 PP_GCM_T(int&
-        ,int&    // GCC
-        ,int &   // Clang
-        ,int &); // MSVC
+      , "int&"   // GCC
+      , "int &"  // Clang
+      , "int&"); // MSVC
 
 PP_GCM_T(int*
-        ,int*    // GCC
-        ,int *   // Clang
-        ,int *); // MSVC
+       ,"int*"   // GCC
+       ,"int *"  // Clang
+       ,"int*"); // MSVC
 
 PP_GCM_T(const int&
-        ,const int&    // GCC
-        ,const int &   // Clang
-        ,const int &); // MSVC
+       ,"const int&"   // GCC
+       ,"const int &"  // Clang
+       ,"const int&"); // MSVC
 
 PP_GCM_T(const int*
-        ,const int*    // GCC
-        ,const int *   // Clang
-        ,const int *); // MSVC
+       ,"const int*"   // GCC
+       ,"const int *"  // Clang
+       ,"const int*"); // MSVC
 
 PP_GCM_T(int* const
-        ,int* const    // GCC
-        ,int *const    // Clang
-        ,int * const); // MSVC
+       ,"int* const"   // GCC
+       ,"int *const"   // Clang
+       ,"int*const "); // MSVC (spacing error)
 
 struct ch { char c; };
-PP_SAME_T(char ch::*);
-static_assert(type_name_pp<decltype(&ch::c)> == "char ch::*");
+PP_GCM_T(char ch::*
+       ,"char ch::*"   // GCC
+       ,"char ch::*"   // Clang
+       ,"charch::* ");  // MSVC (spacing error)
 
 PP_GCM_T(std::nullptr_t
-        ,std::nullptr_t    // GCC
-        ,     nullptr_t    // Clang
-        ,     nullptr);    // MSVC ??
+       ,"std::nullptr_t"  // GCC
+       ,     "nullptr_t"  // Clang
+       ,     "nullptr");  // MSVC (missing _t)
 
 const volatile char abc[1][2][3]{};
 PP_GCM_T(decltype(abc)
-        ,const volatile char [1][2][3]   // GCC
-        ,char const volatile[1][2][3]    // Clang
-        ,volatile const char[1][2][3]);  // MSVC
+       ,"const volatile char [1][2][3]"   // GCC
+       ,"char const volatile[1][2][3]"    // Clang
+       ,"volatile const char[1][2][3]");  // MSVC
 
 // Hello world
 
 namespace Hello { struct World; }
 
 PP_GCM_T(Hello::World
-        ,Hello::World           // GCC
-        ,Hello::World           // Clang
-        ,struct Hello::World);  // MSVC
+       ,"Hello::World"          // GCC
+       ,"Hello::World"          // Clang
+       ,"struct Hello::World"); // MSVC
 
 // Non-type template arg tests
 // Non-types with platform-dependent output
 
 
-constexpr auto zero_pp = ntbs::array{ GCM("0"    // GCC
-                                        , "0"    // Clang
-                                        , "0x0"  // MSVC
-) };
+constexpr auto& zero_pp = GCM("0"      // GCC
+                            , "0"      // Clang
+                            , "0x0");  // MSVC
+
 
 // Different Integral types give the same output (not good)
 static_assert( auto_name_pp<0>       == zero_pp );
@@ -132,10 +135,10 @@ static_assert( auto_name_pp<0U>      == zero_pp );
 static_assert( auto_name_pp<short{}> == zero_pp );
 static_assert( auto_name_pp<long{}>  == zero_pp );
 
-constexpr auto one_pp = ntbs::array{ GCM("1"    // GCC
-                                       , "1"    // Clang
-                                       , "0x1"  // MSVC
-) };
+constexpr auto& one_pp = GCM("1"      // GCC
+                           , "1"      // Clang
+                           , "0x1");  // MSVC
+
 static_assert( auto_name_pp<1>        == one_pp );
 static_assert( auto_name_pp<1U>       == one_pp );
 static_assert( auto_name_pp<short{1}> == one_pp );
@@ -143,47 +146,47 @@ static_assert( auto_name_pp<long{1}>  == one_pp );
 
 
 // Printable char, e.g. '0'
-static_assert( auto_name_pp<'0'> == GCM("'0'"     // GCC
-                                       ,"'0'"     // Clang
-                                       ,"0x30")); // MSVC
+static_assert( auto_name_pp<'0'> == GCM("'0'"       // GCC
+                                       ,"'0'"       // Clang
+                                       , "0x30") ); // MSVC
 // Non-printable char, e.g. 0
-static_assert( auto_name_pp<char{}> ==
-     GCM (""           // GCC>=9, "'\000'" GCC<9
-      , R"('\x00')"    // Clang
-         , "0x0"));   // MSVC
+static_assert( auto_name_pp<char{}> == GCM (""          // GCC
+                                         , R"('\x00')"  // Clang
+                                         , "0x0") );    // MSVC
 
 constexpr char c{};
-static_assert( auto_name_pp<&c> == GCM("(& c)"
-                                      , "&c"
-                                      , "& c"));
+static_assert( auto_name_pp<&c> == GCM("(& c)"    // GCC
+                                      , "&c"      // Clang
+                                      , "& c") ); // MSVC
 
 
-static_assert(auto_name_pp<&ch::c> == GCM("&ch::c"
-                                        , "&ch::c"
-                                        , "pointer-to-member(0x0)"));
+static_assert(auto_name_pp<&ch::c> == GCM("&ch::c"                     // GCC
+                                        , "&ch::c"                     // Clang
+                                        , "pointer-to-member(0x0)") ); // MSVC
 
 
 // Enums
 // Simple cases are fairly consistent (since GCC9).
 
 enum e { a, b };
-static_assert( auto_name_pp<a> == ntbs::array{"a"} );
-static_assert( auto_name_pp<b> == ntbs::array{"b"} );
+static_assert( auto_name_pp<a> == "a" );
+static_assert( auto_name_pp<b> == "b" );
 enum class E { m, n };
-static_assert( auto_name_pp<E::m> == ntbs::array{"E::m"} );
-static_assert( auto_name_pp<E::n> == ntbs::array{"E::n"} );
+static_assert( auto_name_pp<E::m> == "E::m" );
+static_assert( auto_name_pp<E::n> == "E::n" );
 
 enum C : char { y, z };
-static_assert( auto_name_pp<y> == ntbs::array{"y"} );
-static_assert( auto_name_pp<z> == ntbs::array{"z"} );
+static_assert( auto_name_pp<y> == "y" );
+static_assert( auto_name_pp<z> == "z" );
 
 #include <string>
 static_assert( type_name_pp<std::string> ==
-  GCM("std::__cxx11::basic_string<char>"
-    , "std::__1::basic_string<char>"
-    , "class std::basic_string<char, struct std::char_traits<char>,"
-                                     "class std::allocator<char> >"));
+  GCM("std::__cxx11::basic_string<char>"   // GCC
+    , "std::__1::basic_string<char>"          // Clang
+    , "class std::basic_string<char,struct std::char_traits<char>,"   // MSVC
+                                   "class std::allocator<char> >") ); // ...
 
+#include <cstdio>
 int main()
 {
 }
